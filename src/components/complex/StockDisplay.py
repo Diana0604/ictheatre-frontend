07bf8f08ffdc8575dce0a59abfe2a59d83e44a13
Diana0604 <diana.vallverdu@gmail.com>
2022-10-26 12:03:04 +0100

    display layout becomes company name - price - up or down

--- src/components/complex/StockDisplay.py
+++ src/components/complex/StockDisplay.py
@@ -1,12 +1,14 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPalette, QColor
-
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
 
 class StockDisplay(QWidget):
     def __init__(self):
         super().__init__()
-        self.setAutoFillBackground(True)
 
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor('green'))
-        self.setPalette(palette)
+        layout = QHBoxLayout()
+
+        layout.addWidget(QLabel('Company Name'))
+        layout.addWidget(QLabel('$000.0'))
+        layout.addWidget(QLabel('UP'))
+
+
+        self.setLayout(layout)
