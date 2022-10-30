e3886474e47ed9db60745bbc52223fe1ad800871
Diana0604 <diana.vallverdu@gmail.com>
2022-10-30 16:17:28 +0000

    improve thread management

--- src/components/stock/StockList.py
+++ src/components/stock/StockList.py
@@ -8,6 +8,7 @@ from apiGetters import getCompaniesArray
 import time
 import threading
 
+
 class StockList(QWidget):
     def __init__(self):
         super().__init__()
@@ -15,37 +16,45 @@ class StockList(QWidget):
         layout = QGridLayout()
 
         # get companies list from API
-        companiesArray = getCompaniesArray()
+        self.companiesArray = getCompaniesArray()
         # iterate over companies list and display in two columns
         companyPosX = 0
         companyPosY = 0
-        for company in companiesArray:
+        for company in self.companiesArray:
             company["singleStockWidget"] = SingleStock(
                 company["name"], company["currentPricePerShare"])
-            layout.addWidget(company["singleStockWidget"], companyPosX, companyPosY)
+            layout.addWidget(company["singleStockWidget"],
+                             companyPosX, companyPosY)
             # update pos for next company
             if companyPosY == 2:
                 companyPosX = companyPosX + 1
                 companyPosY = 0
             else:
                 companyPosY = 2
-
+        # display layout
         self.setLayout(layout)
 
-        start_time = time.time()
-        interval = 1
-        def updateMethod(i):
-            timeToSleep = start_time + i*interval - time.time()
-            if(timeToSleep) > 0:
-                time.sleep(timeToSleep)
+        # init the updates
+        # stopThread -> let's the thread know when the class is destroyed
+        self.stopUpdateThread = False
+        # start new thread that updates every second
+        updateThread = threading.Thread(target=self.updatePrice, args=[])
+        updateThread.start()
+
+    def __del__(self):
+        self.stopUpdateThread = True
+
+    def updatePrice(self):
+        while True:
+            if self.stopUpdateThread:
+                return
+            # get new list of companies
             newCompaniesArray = getCompaniesArray()
-            for i in range(len(companiesArray)):
-                print('new price')
-                print(newCompaniesArray[i]["currentPricePerShare"])
-                companiesArray[i]["currentPricePerShare"] = newCompaniesArray[i]["currentPricePerShare"]
-                companiesArray[i]["singleStockWidget"].updatePriceDisplay(companiesArray[i]["currentPricePerShare"])
-
-
-        for i in range(100):
-            newThread = threading.Thread(target=updateMethod, args=[i])
-            newThread.start()
+            # iterate over list of companies
+            for i in range(len(self.companiesArray)):
+                newStockPrice = newCompaniesArray[i]["currentPricePerShare"]
+                # first -> update price in our list
+                self.companiesArray[i]["currentPricePerShare"] = newStockPrice
+                # second -> update price that is displayed
+                self.companiesArray[i]["singleStockWidget"].updatePriceDisplay(
+                    newStockPrice)
